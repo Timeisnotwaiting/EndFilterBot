4dd03c548c13e6ccd6240d5382c480b7084726e8
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-28 18:42:10 +0530

    Update AlphaForYashu.py

--- AlphaForYashu.py
+++ AlphaForYashu.py
@@ -8,5 +8,10 @@ async def get_filter_info(_, m):
         return await m.reply("You need to give the filter a name!")
     if l == 2 and not reply:
         return await m.reply("You need to give the filter some content!")
-    if l == 2 and (reply.sticker or reply.animation):
-        
+    if l > 1 and (reply.sticker or reply.animation):
+        _name = m.text.split()[1].strip()
+        file_id = reply.sticker.file_id if reply.sticker else reply.animation.file_id   
+        return details = {name=_name, file=file_id, format=("sticker" if reply.sticker else "animation"), caption=None}
+    if l > 1 and (reply.photo or reply.video or reply.audio):
+        _name = m.text.split(None, 2)[1]
+        file_path = await _.download_media(reply, f"
